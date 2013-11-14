0907bbf86206e15c3e7bac6f3e415c140b3af25d
arsenische <arsenische@gmail.com>
2013-11-15 02:20:16 +0600

    fix active_admin for project

--- app/admin/project.rb
+++ app/admin/project.rb
@@ -5,6 +5,8 @@ ActiveAdmin.register Project do
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # permit_params :list, :of, :attributes, :on, :model
+
+  permit_params :name, :about, :url, :bitcoin_address
   #
   # or
   #
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,2 @@
 class Project < ActiveRecord::Base
-  attr_accessible :name, :about, :url, :bitcoin_address
 end
