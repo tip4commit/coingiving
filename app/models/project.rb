b9b7e746a77d43e191f5a257f41b251fcf764710
Aleksandr Zykov <alexandrz@gmail.com>
2013-11-16 11:05:33 +0100

    validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,13 @@
 class Project < ActiveRecord::Base
+  
+  has_many :deposit_addresses
+  has_many :deposits, :through => :deposit_addresses
+
+  validates :url, uniqueness: true, url: true
+  validates :donation_page_url, uniqueness: true, url: true
+
   def deposit_address user
     user.nil? ? nil : DepositAddress.find_by_project_id_and_sponsor_id(id, user.id)    
   end
 
-  has_many :deposit_addresses
-  has_many :deposits, :through => :deposit_addresses
 end
