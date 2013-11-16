15bb2a3cbc5cae7e88580715181d20b3dda84301
Aleksandr Zykov <alexandrz@gmail.com>
2013-11-16 17:44:33 +0100

    fix

--- app/models/deposit.rb
+++ app/models/deposit.rb
@@ -1,7 +1,7 @@
 class Deposit < ActiveRecord::Base
   belongs_to :deposit_address
-  belongs_to :project, through: :deposit_address
-  belongs_to :sponsor, through: :deposit_address
+  has_one :project, through: :deposit_address
+  has_one :sponsor, through: :deposit_address
 
   scope :by_project, ->(project) { includes(:deposit_address).where('deposit_addresses.project_id = ?', project.id) }
 end
