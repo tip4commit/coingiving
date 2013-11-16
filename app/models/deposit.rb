a0a3f07ecf32d5a3c6efdc9b80ad11947b26db12
arsenische <arsenische@gmail.com>
2013-11-16 22:46:01 +0600

    added delegate to fix error in _deposits.html.slim

--- app/models/deposit.rb
+++ app/models/deposit.rb
@@ -1,5 +1,6 @@
 class Deposit < ActiveRecord::Base
   belongs_to :deposit_address
+  delegate :project, :to => :deposit_address
 
   scope :by_project, ->(project) { includes(:deposit_address).where('deposit_addresses.project_id = ?', project.id) }
 end
