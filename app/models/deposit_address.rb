3e1b2c8e4a4cec56fff33ca239cc09549c664882
arsenische <arsenische@gmail.com>
2013-11-17 01:39:15 +0600

    fixed query

--- app/models/deposit_address.rb
+++ app/models/deposit_address.rb
@@ -3,7 +3,8 @@ class DepositAddress < ActiveRecord::Base
   belongs_to :project
   has_many :deposits
   def update_budget
-    amount = Deposit.where("created_at > '#{Time.now-30.days}' and deposit_address_id=?",1).sum(:amount)
+    amount = Deposit.where(
+      "created_at > '#{Time.now-30.days}' and deposit_address_id=?", id).sum(:amount)
     update_attribute :budget, amount
   end
 end
