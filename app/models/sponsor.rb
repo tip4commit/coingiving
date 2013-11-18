fb476797072cfda45e14a88716e44108a6d9ecfa
Aleksandr Zykov <alexandrz@gmail.com>
2013-11-18 18:33:40 +0100

    comment

--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -15,6 +15,7 @@ class Sponsor < ActiveRecord::Base
   has_many :deposit_addresses
   has_many :deposits, :through => :deposit_addresses
   has_many :projects, -> { where('deposit_addresses.month_donations > 0') }, :through => :deposit_addresses
+  # todo fix to whole time donations ^^^
 
   def to_param
     "#{id}-#{name.parameterize}"
